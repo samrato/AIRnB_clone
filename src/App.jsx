70758f079314f9093c150247768c4731f542c2c4
Willington Juma <willingtonejumamconyango@gmail.com>
2024-11-09 12:40:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./componets/Header";
+import Hero from "./componets/Hero";
+import PracticeScriba from "./componets/PracticeScriba";
+
+
+function App() {
+  
+  return (
+    <div>
+     <Header/>
+     <Hero/>
+      {/* <PracticeScriba/> */}
+    </div>
+
+  );
+}
+
+export default App
